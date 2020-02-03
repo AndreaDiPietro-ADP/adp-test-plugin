bee8551802e51f33cf3c489a1a43c92e4209f0e7
Andrea Di Pietro <himura.battosai.new@gmail.com>
2020-02-03 22:43:57 +0100

    added license

--- adp-test-plugin.php
+++ adp-test-plugin.php
@@ -17,6 +17,8 @@
  * Author: Andrea Di Pietro
  * Text Domain: adp-test-plugin
  * Domain Path:       /languages
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  */
 
 // If this file is called directly, abort.
